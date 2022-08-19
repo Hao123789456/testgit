5776f5ea98eb3c7e05172e8bb0853bd4527eb437
nhh <1749327061@qq.com>
2022-08-19 23:56:42 +0800

    hi commit

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,5 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,word");
         System.out.println("args = " + args);
+        System.out.println("hi");
     }
 }
